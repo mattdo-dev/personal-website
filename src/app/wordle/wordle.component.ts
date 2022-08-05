235c601419c0e66e5032dca479967a65e0ae7c47
Matthew Do <domattgmt7@gmail.com>
2022-08-05 21:02:53 +0700

    adding and testing assets

---
+++ src/app/wordle/wordle.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wordle',
+  templateUrl: './wordle.component.html',
+  styleUrls: ['./wordle.component.sass']
+})
+
+export class WordleComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
